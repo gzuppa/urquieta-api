bc451605d85474a447fc1bccbdfc2fe578881d28
guszuppa <gustavo_zuppa_flores@hotmail.com>
2020-08-16 22:34:32 -0500

    configuring passport & secret key

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb://admin:urquieta123@ds033297.mlab.com:33297/auditivosurquieta" 
+    mongoURI: "mongodb://admin:urquieta123@ds033297.mlab.com:33297/auditivosurquieta",
+    secretOrKey: "urquietaSecret"
   };
