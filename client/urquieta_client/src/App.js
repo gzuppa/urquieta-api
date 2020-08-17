4ded8bb57b902b32c4f08999b4c841da24dd91fa
guszuppa <gustavo_zuppa_flores@hotmail.com>
2020-08-17 12:48:26 -0500

    import dummy components on App.js

--- client/urquieta_client/src/App.js
+++ client/urquieta_client/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import "./App.css"
+import Navbar from './components/layout/Navbar'
+import Landing from './components/layout/Landing'
 
 function App() {
   return (
-    <div>
-      <h1>Hola</h1>
+    <div className="App">
+      <Navbar />
+      <Landing />
     </div>
   );
 }
