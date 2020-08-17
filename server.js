9f71d3a126b2a522127d54157f2a7c0b97bf051f
guszuppa <gustavo_zuppa_flores@hotmail.com>
2020-08-16 22:47:20 -0500

    create passport middleware

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
+
+const users = require("./routes/api/users");
+
 const app = express();
 
 app.use(
@@ -20,5 +24,12 @@ mongoose
   )
   .then(() => console.log("Base de datos corriendo correctamente"))
   .catch(err => console.log(err));
+
+//MIDDLEWARES
+
+app.use(passport.initialize());
+require("./config/passport")(passport);
+app.use("/api/users", users);
+
 const port = process.env.PORT || 5000; 
 app.listen(port, () => console.log(`Servidor para Landing Urquieta corriendo en el puerto ${port} !`));
