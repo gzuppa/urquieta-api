2aff53ba5cda51df4777a18fe3afa0dfa2952299
guszuppa <gustavo_zuppa_flores@hotmail.com>
2020-08-16 22:18:58 -0500

    create validations for login

--- validation/login.js
+++ validation/login.js
@@ -1,34 +1,22 @@
 const Validator = require("validator");
 const isEmpty = require("is-empty");
 
-module.exports = function validateRegisterInput(data) {
+module.exports = function validateLoginInput(data) {
   let errors = {};
-// Se convierten los campos vacios en strings vacios para utilizar el validator
-  data.name = !isEmpty(data.name) ? data.name : "";
+
+// Se convierten campos vacios en strings vacios para utilizar el validator
   data.email = !isEmpty(data.email) ? data.email : "";
   data.password = !isEmpty(data.password) ? data.password : "";
-  data.password2 = !isEmpty(data.password2) ? data.password2 : "";
-// Validaciones de datos
-  if (Validator.isEmpty(data.name)) {
-    errors.name = "Se requiere ingresar un nombre";
-  }
+
   if (Validator.isEmpty(data.email)) {
-    errors.email = "Se requiere ingresar un email";
+    errors.email = "El campo email es requerido";
   } else if (!Validator.isEmail(data.email)) {
     errors.email = "El email es inválido";
   }
   if (Validator.isEmpty(data.password)) {
-    errors.password = "Es requerido colocar un password";
+    errors.password = "El campo password es requerido";
   }
-if (Validator.isEmpty(data.password2)) {
-    errors.password2 = "Favor de confirmar el password";
-  }
-if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
-    errors.password = "El password debe tener un mínimo de 6 caracteres";
-  }
-if (!Validator.equals(data.password, data.password2)) {
-    errors.password2 = "Los passwords deben coincidir";
-  }
+
 return {
     errors,
     isValid: isEmpty(errors)
