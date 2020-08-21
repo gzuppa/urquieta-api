81ba39e4c3f7568a84dbbbd1de1853cefce52ad3
guszuppa <gustavo_zuppa_flores@hotmail.com>
2020-08-20 22:34:59 -0500

    restyling register with functionality

--- client/urquieta_client/src/components/auth/Register.js
+++ client/urquieta_client/src/components/auth/Register.js
@@ -3,6 +3,7 @@ import { Link, withRouter } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { registerUser } from "../../actions/authActions";
+import Assets from '../../assets/Assets'
 import classnames from "classnames";
 
 class Register extends Component {
@@ -50,82 +51,95 @@ class Register extends Component {
     render() {
         const { errors } = this.state;
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col s8 offset-s2">
-            <Link to="/" className="btn-flat waves-effect">
-              <i className="material-icons left">keyboard_backspace</i> Volver al inicio
-            </Link>
-            <div className="col s12" style={{ paddingLeft: "11.250px" }}>
-              <h4>
-                <b>Registro</b> 
-              </h4>
-              <p className="grey-text text-darken-1">
-                ¿Ya tienes una cuenta? <Link to="/login">Ingresa aqui</Link>
-              </p>
-            </div>
-            <form noValidate onSubmit={this.onSubmit}>
-              <div className="input-field col s12">
-                <input
+        <div className="limiter">
+		<div className="container-login">
+			<div className="wrap-login">
+        <form noValidate 
+              onSubmit={this.onSubmit} 
+              className="login-form validate-form">
+          <span className="logo">
+            <img src={Assets.authImgs.avatar} 
+                 alt="logo" 
+                 className="avatar"/>
+		  </span>
+                    <span className="login-form-title">
+						Registro
+					</span>
+                    <p className="txt4">
+                        ¿Ya tienes una cuenta? <Link to="/login">Ingresa aqui</Link>
+                    </p>
+
+		<div className="wrap-input validate-input">
+            <input 
                   onChange={this.onChange}
                   value={this.state.name}
                   error={errors.name}
                   id="name"
+                  className={classnames("input", { invalid: errors.name })}
                   type="text"
-                  className={classnames("", { invalid: errors.name })}
-                />
-                <label htmlFor="name">Nombre</label>
-                <span className="red-text">{errors.name}</span>
-              </div>
-              <div className="input-field col s12">
-                <input
+                  placeholder="Nombre" />
+                <span className="red-text">
+                  {errors.name}
+                </span>
+		</div>
+
+		<div className="wrap-input validate-input">
+            <input 
                   onChange={this.onChange}
                   value={this.state.email}
                   error={errors.email}
                   id="email"
+                  className={classnames("input", { invalid: errors.email })}
                   type="email"
-                  className={classnames("", { invalid: errors.email })}
-                />
-                <label htmlFor="email">Email</label>
-                <span className="red-text">{errors.email}</span>
-              </div>
-              <div className="input-field col s12">
-                <input
+                  placeholder="Email" />
+                <span className="red-text">
+                  {errors.email}
+                </span>
+		</div>
+
+        <div className="wrap-input validate-input">
+            <input 
                   onChange={this.onChange}
                   value={this.state.password}
                   error={errors.password}
                   id="password"
+                  className={classnames("input", { invalid: errors.password })}
                   type="password"
-                  className={classnames("", { invalid: errors.password })}
-                />
-                <label htmlFor="password">Password</label>
-                <span className="red-text">{errors.password}</span>
-              </div>
-              <div className="input-field col s12">
-                <input
+                  placeholder="Password" />
+                <span className="red-text">
+                  {errors.name}
+                </span>
+		</div>
+
+        <div className="wrap-input validate-input">
+            <input 
                   onChange={this.onChange}
                   value={this.state.password2}
                   error={errors.password2}
                   id="password2"
+                  className={classnames("input", { invalid: errors.password2 })}
                   type="password"
-                  className={classnames("", { invalid: errors.password2 })}
-                />
-                <label htmlFor="password2">Confirma tu password</label>
-                <span className="red-text">{errors.password2}</span>
-              </div>
-              <div className="col s12" style={{ paddingLeft: "11.250px" }}>
-                <button
-                  style={{ width: "150px", borderRadius: "3px", letterSpacing: "1.5px", marginTop: "1rem" }}
-                  type="submit"
-                  className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-                >
-                  Registrate
-                </button>
-              </div>
-            </form>
-          </div>
+                  placeholder="Confirma tu password" />
+                <span className="red-text">
+                  {errors.password2}
+                </span>
+		</div>
+
+		<div className="container-login-form-btn">
+			<button className="login-form-btn" type="submit">
+				Registro
+			</button>
+		</div>
+
+        <div>
+            <Link to="/" className="btn-flat waves-effect">
+              <span className="txt3">Volver al inicio</span>
+            </Link>
         </div>
-      </div>
+				</form>
+			</div>
+		</div>
+	</div>
     );
   }
 }
