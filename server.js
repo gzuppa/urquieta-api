e434f9d6b9d178b881d16c68946696c962c249b3
guszuppa <gustavo_zuppa_flores@hotmail.com>
2020-08-31 03:10:39 -0500

    create GET endpoint to list uploaded images

--- server.js
+++ server.js
@@ -43,6 +43,17 @@ if(process.env.NODE_ENV === 'production') {
   })
 }
 
+app.get('/api/images', async (req, res) => {
+  const {resources} = await cloudinary.search
+                            .expression('folder:dev_setups')
+                            .sort_by('public_id', 'desc')
+                            .max_results(30)
+                            .execute()
+  const publicIds = resources.map( file => file.
+    public_id)
+    res.send(publicIds)
+})
+
 app.post('/api/upload', async (req, res) => {
   try {
     const fileStr = req.body.data
