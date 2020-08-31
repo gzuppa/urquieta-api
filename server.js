128de6024828aa7e7d9b5a2f197adaccab384cc6
guszuppa <gustavo_zuppa_flores@hotmail.com>
2020-08-31 02:21:26 -0500

    set up POST endpoint for images

--- server.js
+++ server.js
@@ -31,6 +31,8 @@ mongoose
 app.use(passport.initialize());
 require("./config/passport")(passport);
 app.use("/api/users", users);
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ limit: '50mb', extended: true }))
 
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
@@ -38,6 +40,15 @@ if(process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
+
+app.post('/api/upload', (req, res) => {
+  try {
+    const fileStr = req.body.data
+    console.log(fileStr)
+  } catch(error) {
+    console.error(error)
+  }
+})
 
 const port = process.env.PORT || 5000; 
 app.listen(port, () => console.log(`Servidor para Landing Urquieta corriendo en el puerto ${port} !`));
