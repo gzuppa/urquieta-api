1a1c6a0df0eb0033e28ff39114b536591219312e
guszuppa <gustavo_zuppa_flores@hotmail.com>
2020-09-03 23:48:15 -0500

    navbar fixed, advisory&sidebar fixed

--- client/urquieta_client/src/components/landing/FreeAdvisory.js
+++ client/urquieta_client/src/components/landing/FreeAdvisory.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Grid, Image } from 'semantic-ui-react'
+import { Grid, Image, List } from 'semantic-ui-react'
 import Assets from '../../assets/Assets'
 import './landing.css'
 
@@ -15,7 +15,20 @@ const FreeAdvisory = () => (
         <Image src={Assets.generalImgs.advisory} className="img-advisory" />
       </Grid.Column>
       <Grid.Column width={2}>
-        <Image src={Assets.generalImgs.advisory} className="img-advisory" />
+          <List animated verticalAlign='middle' className="sidebar-list">
+            <List.Item>
+              <Image className="sidebar-icon" src={Assets.generalImgs.whatsapp} alt="whatsapp" />
+            </List.Item>
+            <List.Item>
+              <Image className="sidebar-icon" src={Assets.generalImgs.agenda} alt="agenda" />
+            </List.Item>
+            <List.Item>
+              <Image className="sidebar-icon" src={Assets.generalImgs.llamada} alt="llamada"/>
+            </List.Item>
+            <List.Item>
+              <Image className="sidebar-icon" src={Assets.generalImgs.chat} alt="chat"/>
+            </List.Item>
+      </List>
       </Grid.Column>
     </Grid.Row>
     </Grid>
