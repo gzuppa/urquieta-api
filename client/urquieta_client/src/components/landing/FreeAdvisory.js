3f473f6234450daa45dd94b22cc6ecea580eef76
guszuppa <gustavo_zuppa_flores@hotmail.com>
2020-09-03 20:16:38 -0500

    advisory alignment

--- client/urquieta_client/src/components/landing/FreeAdvisory.js
+++ client/urquieta_client/src/components/landing/FreeAdvisory.js
@@ -6,12 +6,15 @@ import './landing.css'
 const FreeAdvisory = () => (
     <Grid>
     <Grid.Row>
-      <Grid.Column width={8}>
+      <Grid.Column width={6}>
         <p className="advisory">Asesoría gratis</p>
         <p className="advisory-sub">¿Quieres saber más sobre la pérdida auditiva?</p>
         <p className="advisory-2">Click aquí</p>
       </Grid.Column>
       <Grid.Column width={8}>
+        <Image src={Assets.generalImgs.advisory} className="img-advisory" />
+      </Grid.Column>
+      <Grid.Column width={2}>
         <Image src={Assets.generalImgs.advisory} className="img-advisory" />
       </Grid.Column>
     </Grid.Row>
