afbb7fe85e691692a796d9f874f9dfb809d6fbdc
guszuppa <gustavo_zuppa_flores@hotmail.com>
2020-09-11 18:28:24 -0500

    cards resposive iPhone Plus

--- client/urquieta_client/src/components/landing/CardSection.js
+++ client/urquieta_client/src/components/landing/CardSection.js
@@ -5,7 +5,53 @@ import './landing.css'
 
 const CardSection = () => (
     <Grid columns='equal'>
-    <Grid.Row>
+    <Grid.Row columns={3} only="computer">
+      <Grid.Column>
+      <Card color="blue" link className="card-devices">
+            <Image src={Assets.generalImgs.card1} className="card-photo"/>
+            <Card.Content>
+            <Card.Header className="card-header">Intracanales</Card.Header>
+            <Card.Description className="card-desc">
+                Se coloca dentro del canal auditivo de forma discreta.
+                Son adecuados para pérdida auditiva de leve a ligeramente severa.
+                Se moldea para colocarse en el interior del conducto 
+                auditivo adaptandose a la perfección                                                                     
+            </Card.Description>
+            </Card.Content>
+        </Card>
+      </Grid.Column>
+      <Grid.Column>
+      <Card color="blue" link className="card-devices">
+            <Image src={Assets.generalImgs.card2} className="card-photo" />
+            <Card.Content>
+            <Card.Header className="card-header">Curveta</Card.Header>
+            <Card.Description className="card-desc">
+                Mayor potencia con una batería de larga duración. Hay
+                curvetas de diferentes tamaños y pueden cubrir desde la
+                baja auditiva más leve hasta la más profunda. Existe una 
+                generación de curvetas mini, que se esconden tras la 
+                oreja.
+            </Card.Description>
+            </Card.Content>
+        </Card>
+      </Grid.Column>
+      <Grid.Column>
+      <Card color="blue" link className="card-devices">
+            <Image src={Assets.generalImgs.card3} className="card-photo" />
+            <Card.Content>
+            <Card.Header className="card-header">Sistemas de vibración osea</Card.Header>
+            <Card.Description className="card-desc">
+                El sistema vibratorio se coloca detrás de la oreja, transforma
+                las ondas sonoras en vibraciones y así amplifica el sonido. 
+                Estimula directamente a la cóclea o caracol sin necesidad de 
+                que pase el sonido por el conducto auditivo.
+            </Card.Description>
+            </Card.Content>
+        </Card>
+      </Grid.Column>
+    </Grid.Row>
+
+    <Grid.Row columns={1} only="mobile">
       <Grid.Column>
       <Card color="blue" link className="card-devices">
             <Image src={Assets.generalImgs.card1} className="card-photo"/>
